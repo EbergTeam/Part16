558812fb4a92ca01d0dcab1ba97c93ca8cb4d0ac
Мирьян <miryan.asmanov@mail.ru>
2022-01-21 16:24:01 +0500

    Add project files.

---
+++ Part16/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Part16.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Part16.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Person person)
+        {
+            // Объект ModelState сохраняет все значения, которые пользователь ввел для свойств модели,
+            // а также все ошибки, связанные с каждым свойством и с моделью в целом.
+            if (ModelState.IsValid)
+                return Content($"{person.Name} - {person.Email}");
+            else
+                return View(person);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Part16/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Part16.Models
+{
+    public class Person
+    {
+        [Required(ErrorMessage = "Не указано Имя")] // атрибут Required, благодаря чему фреймворк знает, что данное свойство обязательно должно содержать некоторое значение
+        public string Name { get; set; }
+        [Required] 
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Длина от 3 до 50")] // максимальная допустимая длина строки
+        [EmailAddress(ErrorMessage = "Некорректный адрес")]
+        public string Email { get; set; }
+        [Required] // атрибут Required, благодаря чему фреймворк знает, что данное свойство обязательно должно содержать некоторое значение
+        public string Password { get; set; }
+        [Range(18,150, ErrorMessage = "Возраст от 18")] // минимальные и максимальные ограничения для числовых данных
+        // [Remote(action: "CheckEmail", controller: "Home", ErrorMessage ="Email уже используется")]
+        // Для валидации свойства выполняет запрос на сервер к определенному методу контроллера.
+        // И если требуемый метод контроллера вернет  значение false, то валидация не пройдена
+        public int Age { get; set; }
+    }
+}
